b2a80e3a68ebde03235ffa21d83a1933d4116f88
Bhumika Saini <bs98.saini@gmail.com>
2018-04-11 00:10:58 +0530

    href bugs fixed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function showCards(id1, id2) {
     var x = document.getElementById(id1);
     var x1 = document.getElementById(id2);
-    if (x.style.display === "none") {
+    if (x.style.display !== "flex") {
         x.style.display = "flex";
-        x1.style.display = "none";
-    } else {
-        x.style.display = "none";
+        if (x1.style.display === "flex") {
+        	x1.style.display = "none";
+        }
     }
 } 
